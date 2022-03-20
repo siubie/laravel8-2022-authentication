189b5bd4f79e670e46fe112be92222e1e01de9aa
dwielok <dwieloknuraini773@gmail.com>
2022-03-20 20:16:02 +0700

    meperbaiki seeder

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,7 +45,7 @@
 
                             <div class="row mb-3">
                                 <label for="email"
-                                    class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                                    class="col-md-4 col-form-label text-md-end">{{ __('E-Mail Address') }}</label>
 
                                 <div class="col-md-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
