774d21c01dc863059966b04704d8b03a5d234c71
AndreMaulanaMustofa <andremm73@gmail.com>
2022-03-21 08:03:25 +0700

    New Controller Added

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    protected $redirectTo = RouteServiceProvider::HOME;
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function username(){
+        return 'username';
+    }
+}
