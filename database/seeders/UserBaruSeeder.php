0d476f2ca2d05efe259cd2931c484c5b458e604e
K2FA <khalidarrafi6331@gmail.com>
2022-03-21 14:26:41 +0700

    Change Login using username

--- database/seeders/UserBaruSeeder.php
+++ database/seeders/UserBaruSeeder.php
@@ -1,11 +1,12 @@
 <?php
+
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class UserBaruSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -16,8 +17,9 @@ class UserSeeder extends Seeder
     {
         //
         DB::table('users')->insert([
-            'name' => 'Administrator',
-            'email'=> 'admin@admin.com',
+            'username'=>'admin',
+            'name' => 'Administrator Baru',
+            'email'=> 'admin.baru@admin.com',
             'password'=>Hash::make('password'),
         ]);
     }
