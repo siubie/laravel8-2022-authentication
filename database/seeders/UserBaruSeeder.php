cca17c1b1f6061ec307e28148a890dedbf099a3e
ahmad alfaruq <ahmadalfaruq098@gmail.com>
2022-03-22 18:02:11 +0700

    Praktikum 3:Change login using username

--- database/seeders/UserBaruSeeder.php
+++ database/seeders/UserBaruSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class UserSeeder extends Seeder
+class UserBaruSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -17,12 +17,11 @@ class UserSeeder extends Seeder
     {
         //
         DB::table('users')-> insert([
-            'name' => 'Administrator',
-            'email' => 'admin@admin.com',
+            'username' => 'admin',
+            'name' => 'Administrator Baru',
+            'email' => 'admin.baru@admin.com',
             'password' => Hash::make('password'),
-            'created_at'=> now()
         ]);
-
 
     }
 }
